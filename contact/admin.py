41a869ee5a58868a199d57a5e1737b14b940e737
Lucas Fuzinaga <lucasfuzinagacat@gmail.com>
2023-07-19 10:07:59 -0300

    criei admin do model contatos e criei contatos pelo shell

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from contact.models import Contact
 # Register your models here.
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'phone'
+    ordering = '-id',
+    search_fields = 'id', 'first_name', 'last_name'
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = ('first_name',)
+    list_display_links = ('id',)
